001693a95a7c6c6c5ae950b7df3465e8a50f4944
Paramjeet Dhanjal <paramjeetkaurdhanjal8@gmail.com>
2026-05-14 13:05:21 +0530

    refactor: remove redundant comments regarding environment and host configuration in main.py

--- main.py
+++ main.py
@@ -9,7 +9,5 @@ if __name__ == '__main__':
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
     
-    # Get the port from the system (Mini Deploy uses 8080, local uses 5001)
     port = int(os.environ.get("PORT", 5001))
-    # You MUST use host="0.0.0.0" so Docker can "see" your app
     app.run(host="0.0.0.0", port=port, debug=True)
